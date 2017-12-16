a654cd170a3f934e1a0626eb126c972668e65d24
Tiago Henrique da Silva <thiago.silva@ingresso.com>
2017-12-16 14:37:02 -0200

    feature(modules): create architecture for the project

---
+++ domain/src/main/java/com/example/raphaelsantos/toplistsapp/domain/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.example.raphaelsantos.toplistsapp.domain.repository;
+
+import java.util.List;
+
+/**
+ * Created by Tiago on 16/12/2017.
+ */
+
+public interface MovieRepository {
+
+    List<Object> findAll();
+}
---
+++ domain/src/main/java/com/example/raphaelsantos/toplistsapp/domain/repository/MovieRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.example.raphaelsantos.toplistsapp.domain.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieRepositoryImpl implements MovieRepository {
+
+    @Override
+    public List<Object> findAll() {
+        return new ArrayList<>();
+    }
+}
